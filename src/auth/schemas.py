78f2f70abbb0f8c5e10c5824898b6d4beb6ad882
Student <student@compass.local>
2026-04-08 10:36:26 +0600

    commit

---
+++ src/auth/schemas.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from pydantic import BaseModel, model_validator
+
+
+class UserRegisterSchema(BaseModel):
+    email: str
+    password: str
+    password_2: str
+
+
+
+    @model_validator(mode='after')
+    def check_passwords_match(self):
+        if self.password != self.password_2:
+            raise ValueError('Passwords no match')
+        return self
+
+class UserSchema(BaseModel):
+    email: str
+    password: str
+    first_name: str
+    last_name: str
